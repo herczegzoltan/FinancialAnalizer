64d304d19eb2235156057590575eb015d6e5a7f6
Zoltan Herczeg <zoltan.herczeg>
2020-03-24 21:05:26 +0100

    Add project files.

---
+++ FinancialAnalizer/Model/Record.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialAnalizer.Model
+{
+    public class Record
+    {
+        public int MyProperty { get; set; }
+
+    }
+}
---
+++ FinancialAnalizer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialAnalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var reader = new StreamReader(@"C:\Users\Herczeg Zoltán\Google Drive\input.csv"))
+            {
+                //List<string> listA = new List<string>();
+                //List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    Console.WriteLine(values[0]);
+                    //listA.Add(values[0]);
+                    //listB.Add(values[1]);
+                }
+            }
+        }
+    }
+}
